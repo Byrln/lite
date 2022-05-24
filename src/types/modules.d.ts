1ad385529428a5f99072026b285d2bedc2e11d1a
nb <nb685858@gmail.com>
2022-05-24 13:28:53 +0800

    update

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -1,5 +1,3 @@
-// declare module "less-vars-to-js";
-// declare module "!!raw-loader!styles/antd-custom.less";
 declare module "react-calendar-timeline/lib";
 declare module "lodash";
 declare module "numeral";
