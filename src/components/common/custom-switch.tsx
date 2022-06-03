53fa5b1abafe0b1919b2bb468a02d38d64dd8ef8
nb <nb685858@gmail.com>
2022-06-03 13:38:17 +0800

    update

---
+++ src/components/common/custom-switch.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { mutate } from "swr";
+import { Switch } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+
+const ToggleChecked = ({ id, checked, api, apiUrl, mutateUrl }: any) => {
+    const [isChecked, setIsChecked] = useState(checked);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setIsChecked(checked);
+    }, [checked]);
+
+    const onToggleChecked = async () => {
+        setLoading(true);
+
+        try {
+            await api.toggleChecked(id, !checked, apiUrl);
+            mutateUrl && (await mutate(mutateUrl));
+
+            setIsChecked(!isChecked);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <LoadingButton loading={loading}>
+            <Switch
+                checked={isChecked}
+                disabled={loading}
+                onClick={onToggleChecked}
+            />
+        </LoadingButton>
+    );
+};
+
+export default ToggleChecked;
