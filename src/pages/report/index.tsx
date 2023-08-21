8c09058ae3291237c8f66241ba904b5a628c669f
Amaraa Baasanjav <amaraa.b@cumongol.mn>
2023-08-21 17:42:54 +0800

    fix: changes

--- src/pages/report/index.tsx
+++ src/pages/report/index.tsx
@@ -2,7 +2,6 @@ import { Box, Grid, Container, Typography } from "@mui/material";
 import Head from "next/head";
 
 import Page from "components/page";
-import Document from "next/document";
 
 const title = "Зочин бүртгэл";
 
@@ -20,11 +19,9 @@ const Index = () => (
                 </Box>
                 <Grid container spacing={3}>
                     <Grid item xs={12}>
-                        <iframe src={'blob:http://localhost:5488/36d37eb8-fef2-4515-995f-339b2328dd47'}></iframe>
-                        <iframe id="if1" width="100%" height="900" src="https://www.africau.edu/images/default/sample.pdf"></iframe>
-                        <object data="{'blob:http://localhost:5488/36d37eb8-fef2-4515-995f-339b2328dd47'}" type="application/pdf">
-                            <embed src="{'blob:http://localhost:5488/36d37eb8-fef2-4515-995f-339b2328dd47'}" type="application/pdf" />
-                        </object>
+                        <iframe width={800} height={600} onload="this.width=screen.width;this.height=screen.height;"
+                            src="http://124.158.124.85/ReportServer/Pages/ReportViewer.aspx?%2fInterAgency&rs:Command=Render&rs:Embed=True"
+                            type="application/html" />
                     </Grid>
                 </Grid>
             </Container>
