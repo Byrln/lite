134a427d5ae4dd5a82520dd13e7e25e0cba41631
nearUNB <near1111.nn@gmail.com>
2024-03-29 16:56:16 +0800

    update

--- src/components/transaction/folio/edit-transaction.tsx
+++ src/components/transaction/folio/edit-transaction.tsx
@@ -18,15 +18,20 @@ import { mutate } from "swr";
 
 import { CurrencySWR } from "lib/api/currency";
 
-export default function EditFolioTransaction({FolioID, CurrID, TypeID, handleModal, TransactionID}:any){
-
+export default function EditFolioTransaction({
+    FolioID,
+    CurrID,
+    TypeID,
+    handleModal,
+    TransactionID,
+}: any) {
     useEffect(() => {
         fetchDate();
     }, []);
 
-    const {data} =CurrencySWR();
+    const { data } = CurrencySWR();
 
-    const exhcangechoice=data;
+    const exhcangechoice = data;
 
     const [workingDate, setWorkingDate] = useState(null);
 
@@ -46,26 +51,25 @@ export default function EditFolioTransaction({FolioID, CurrID, TypeID, handleMod
         }
     };
 
-    const [quantity, setQuantity]=useState<any>(0)
+    const [quantity, setQuantity] = useState<any>(0);
 
-    const [iName, setIName]=useState();
+    const [iName, setIName] = useState();
 
-    const [tType, setTType]=useState<any>(0);
+    const [tType, setTType] = useState<any>(0);
 
-    const [amount, setAmount]=useState<any>(0);
+    const [amount, setAmount] = useState<any>(0);
 
-    const [amount2, setAmount2]=useState<any>(0);
+    const [amount2, setAmount2] = useState<any>(0);
 
-    const [currency, setCurrency]=useState<any>('');
+    const [currency, setCurrency] = useState<any>("");
 
-    const [currencyID, setCurrencyID]=useState<any>('')
+    const [currencyID, setCurrencyID] = useState<any>("");
 
-    const [currency2, setCurrency2]=useState<any>('');
+    const [currency2, setCurrency2] = useState<any>("");
 
-    const [EditableRate, setEditableRate]=useState<any>('');
+    const [EditableRate, setEditableRate] = useState<any>("");
 
-    const [description, setDescription]=useState<any>(' ');
-
+    const [description, setDescription] = useState<any>(" ");
 
     const fetchDate = async () => {
         let response = await FrontOfficeAPI.workingDate();
@@ -73,52 +77,52 @@ export default function EditFolioTransaction({FolioID, CurrID, TypeID, handleMod
             setWorkingDate(response.workingDate[0].WorkingDate);
         }
     };
-    
+
     useEffect(() => {
         fetchDatas();
     }, [FolioID, CurrID, TypeID]);
 
-    const [useData, setUseData]=useState()
+    const [useData, setUseData] = useState();
 
-    console.log(useData)
+    console.log(useData);
 
     const fetchDatas = async () => {
-        let response = await FolioAPI.edits(FolioID , CurrID ,TypeID);
+        let response = await FolioAPI.edits(FolioID, CurrID, TypeID);
 
-        setIName(response[0].ItemName)
+        setIName(response[0].ItemName);
 
-        setTType(response[0].TypeID)
+        setTType(response[0].TypeID);
 
-        setCurrency(response[0].PayCurrencyCode)
+        setCurrency(response[0].PayCurrencyCode);
 
-        setCurrencyID(response[0].PayCurrencyID.toString())
+        setCurrencyID(response[0].PayCurrencyID.toString());
 
-        setCurrency2(response[0].CurrencyCode)
+        setCurrency2(response[0].CurrencyCode);
 
-        setEditableRate(response[0].EditableRate)
+        setEditableRate(response[0].EditableRate);
 
         setUseData(response);
-        if(response[0].Quantity==0 && response){
-            setQuantity(1)
-        }else{
-            setQuantity(response[0].Quantity)
+        if (response[0].Quantity == 0 && response) {
+            setQuantity(1);
+        } else {
+            setQuantity(response[0].Quantity);
         }
 
-        if(response[0].Amount1>0 && response){
-            setAmount(response[0].Amount1)
-            setAmount2(response[0].Amount2)
-        }else{
-            if(response[0].Amount1==0){
-                setAmount(response[0].Amount1)
-                setAmount2(response[0].Amount2)
-            }else{
-                setAmount((response[0].Amount1)*-1)
-                setAmount2(response[0].Amount2*-1)
+        if (response[0].Amount1 > 0 && response) {
+            setAmount(response[0].Amount1);
+            setAmount2(response[0].Amount2);
+        } else {
+            if (response[0].Amount1 == 0) {
+                setAmount(response[0].Amount1);
+                setAmount2(response[0].Amount2);
+            } else {
+                setAmount(response[0].Amount1 * -1);
+                setAmount2(response[0].Amount2 * -1);
             }
         }
-    }
+    };
 
-    const handleSubmit= async()=>{
+    const handleSubmit = async () => {
         await FolioAPI?.update({
             TransactionID: TransactionID,
             FolioID: FolioID,
@@ -128,130 +132,129 @@ export default function EditFolioTransaction({FolioID, CurrID, TypeID, handleMod
             PayCurrencyID: currencyID,
             Amount: amount,
             Quantity: quantity,
-            Description: description,}
-        );
+            Description: description,
+        });
         await mutate(`/api/Folio/Items`);
-        handleModal()
-    }
+        handleModal();
+    };
 
     const handleExchangePick = (event: SelectChangeEvent) => {
         setCurrencyID(event.target.value as string);
-        
-       
-    }
+    };
 
-    return<div>
-<LocalizationProvider
-dateAdapter={AdapterDateFns}
-adapterLocale={mn}
->
-<Stack direction='column' justifyContent='center' width='100%' spacing={2}>
+    return (
+        <div>
+            <LocalizationProvider
+                //@ts-ignore
+                dateAdapter={AdapterDateFns}
+                adapterLocale={mn}
+            >
+                <Stack
+                    direction="column"
+                    justifyContent="center"
+                    width="100%"
+                    spacing={2}
+                >
+                    <Stack direction="row" spacing={3} alignItems="center">
+                        <Typography>Date</Typography>
+                        <Typography>{workingDate}</Typography>
+                    </Stack>
+                    <Stack direction="row" spacing={3} alignItems="center">
+                        <Typography>Name</Typography>
+                        <Typography>{iName}</Typography>
+                    </Stack>
+                    {tType == 1 ? (
+                        <div>
+                            <Stack direction="row" spacing={3}>
+                                <Typography>Quantity</Typography>
 
-    <Stack direction='row' spacing={3} alignItems='center'>
-    <Typography>
-        Date
-    </Typography>
-    <Typography>
-        {workingDate}
-    </Typography>
-    </Stack>
-    <Stack direction='row' spacing={3} alignItems='center'>
-    <Typography>
-        Name
-    </Typography>
-    <Typography>
-        {iName}
-    </Typography>
-    </Stack>
-    {
-        (tType==1)? <div>
-<Stack direction='row' spacing={3}>
-    <Typography>
-        Quantity
-    </Typography>
-
-    <TextField type="number" value={quantity} onChange={(newvalue: any) =>setQuantity(newvalue.target.value)}/>
-
-    </Stack>
-        </div>:<div>
-        <Select value={currencyID} 
-                onChange={handleExchangePick} >
-                                    {
-                                        exhcangechoice?.map((element:any)=>{
-                                            return(
-                                            <MenuItem 
+                                <TextField
+                                    type="number"
+                                    value={quantity}
+                                    onChange={(newvalue: any) =>
+                                        setQuantity(newvalue.target.value)
+                                    }
+                                />
+                            </Stack>
+                        </div>
+                    ) : (
+                        <div>
+                            <Select
+                                value={currencyID}
+                                onChange={handleExchangePick}
+                            >
+                                {exhcangechoice?.map((element: any) => {
+                                    return (
+                                        <MenuItem
                                             key={element.CurrencyID}
                                             value={element.CurrencyID}
-                                            >
+                                        >
                                             {element.CurrencyCode}
-                                            </MenuItem>);
-                                        })
-                                    }
+                                        </MenuItem>
+                                    );
+                                })}
+                            </Select>
+                        </div>
+                    )}
 
-                                    </Select>
+                    <Stack direction="row" spacing={3} alignItems="center">
+                        <Typography>Amount</Typography>
+                        <Stack direction="row" spacing={1} alignItems="center">
+                            <TextField
+                                type="number"
+                                disabled={!EditableRate}
+                                value={amount}
+                                onChange={(newvalue: any) =>
+                                    setAmount(newvalue.target.value)
+                                }
+                            />
+                            {tType == 1 ? (
+                                <div>
+                                    <Typography>{currency}</Typography>
+                                </div>
+                            ) : (
+                                <div></div>
+                            )}
+                        </Stack>
+                        {amount == amount2 ? (
+                            <div></div>
+                        ) : (
+                            <div>
+                                <Typography>
+                                    {amount2.toLocaleString()}
+                                    {currency2}
+                                </Typography>
+                            </div>
+                        )}
+                    </Stack>
+
+                    <TextField
+                        value={description}
+                        onChange={(newvalue: any) => setDescription(newvalue)}
+                        fullWidth
+                        multiline
+                        rows={3}
+                    />
+
+                    <Stack direction="row" alignItems="center" spacing={3}>
+                        <Typography>Date</Typography>
+
+                        <Checkbox
+                            checked={chekedTrue}
+                            onChange={handleChekbox}
+                        />
+
+                        <DateTimePicker
+                            disabled={enableDate}
+                            value={setedDate}
+                            onChange={(newValue: any) => setSetedDate(newValue)}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </Stack>
+
+                    <Button onClick={handleSubmit}>Save</Button>
+                </Stack>
+            </LocalizationProvider>
         </div>
-    }
-    
-    <Stack direction='row' spacing={3} alignItems='center'>
-    <Typography>
-        Amount
-    </Typography>
-    <Stack direction='row' spacing={1} alignItems='center'>
-
-    <TextField type="number" disabled={!EditableRate} value={amount} onChange={(newvalue: any) =>(setAmount(newvalue.target.value))}/>
-    {
-        (tType==1)? <div><Typography>
-        {currency}
-    </Typography>
-    </div>:<div>
-
-    </div>
-    }
-    
-
-
-    </Stack>
-  {  
-(amount==amount2)?<div></div>:<div>
-<Typography>
-        {amount2.toLocaleString()}{currency2}
-    </Typography>
-</div>
-    }
-    
-    </Stack>
-
-    
-    <TextField value={description}
-    onChange={(newvalue: any) =>
-        setDescription(newvalue)
-    }
-    fullWidth
-    multiline
-    rows={3}/>
-
-    <Stack direction='row' alignItems='center' spacing={3}>
-
-    <Typography>Date</Typography>
-
-<Checkbox checked={chekedTrue} onChange={handleChekbox} />
-
-
-    <DateTimePicker 
-    disabled={enableDate}
-    value={setedDate}
-    onChange={(newValue: any) => setSetedDate(newValue)}
-                        renderInput={(params) => (
-                            <TextField {...params} />
-                        )}/>
-    </Stack>
-
-    <Button onClick={handleSubmit}>
-        Save
-    </Button>
-
-</Stack>
-
-</LocalizationProvider>
-    </div>
+    );
 }
