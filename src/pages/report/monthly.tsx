fe578f6899e0c37c3e63a47c380ea53b80ffd798
nearUNB <near1111.nn@gmail.com>
2024-04-12 15:49:47 +0800

    update

--- src/pages/report/monthly.tsx
+++ src/pages/report/monthly.tsx
@@ -1,39 +1,62 @@
+import { useEffect, useState } from "react";
 import { Box, Grid, Container, Typography } from "@mui/material";
 import Head from "next/head";
 
+import Month from "components/reporting/month";
 import Page from "components/page";
+import { FrontOfficeAPI } from "lib/api/front-office";
 
 const title = "Сарын тайлан";
 
 // @ts-ignore
-const Index = () => (
-    <>
-        <Head>
-            <title>{title}</title>
-        </Head>
+const Index = () => {
+    const [workingDate, setWorkingDate]: any = useState(null);
 
-        <Page>
-            <Container maxWidth="xl">
-                <Box sx={{ pb: 1 }}>
-                    <Typography variant="h6">{title}</Typography>
-                </Box>
-                <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                        <iframe
+    useEffect(() => {
+        fetchDatas();
+    }, []);
+
+    const fetchDatas = async () => {
+        let response = await FrontOfficeAPI.workingDate();
+        if (response.status == 200) {
+            setWorkingDate(response.workingDate[0].WorkingDate);
+        }
+    };
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+            </Head>
+
+            <Page>
+                <Container maxWidth="xl">
+                    <Box sx={{ pb: 1 }}>
+                        <Typography variant="h6">{title}</Typography>
+                    </Box>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                            {/* <iframe
                             width={"100%"}
                             height={600}
                             // @ts-ignore
                             onLoad="this.width=screen.width;this.height=screen.height;" // @ts-ignore
-                            src={`https://reporting.horecasoft.mn/ReportServer/Pages/ReportViewer.aspx?%2fMonthly&rs:Command=Render&rs:Embed=True&DatabaseName=HotelDB_${localStorage.getItem(
+                            src={`https://reporting.horecasoft.mn/ReportServer/Pages/ReportViewer.aspx?%2fBalance&rs:Command=Render&rs:Embed=True&DatabaseName=HotelDB_${localStorage.getItem(
                                 "hotelId"
                             )}`}
                             type="application/html"
-                        />
+                        /> */}
+                            {workingDate && (
+                                <Month
+                                    title={title}
+                                    workingDate={workingDate}
+                                />
+                            )}
+                        </Grid>
                     </Grid>
-                </Grid>
-            </Container>
-        </Page>
-    </>
-);
+                </Container>
+            </Page>
+        </>
+    );
+};
 
 export default Index;
